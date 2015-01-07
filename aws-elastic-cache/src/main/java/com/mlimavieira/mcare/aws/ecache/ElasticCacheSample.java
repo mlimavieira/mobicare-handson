eb272620f86206777dfb6e3c6096854312a0e68e
Marcio Vieira <mlimavieira@gmail.com>
2015-01-07 17:32:10 -0200

    HandsOn

---
+++ aws-elastic-cache/src/main/java/com/mlimavieira/mcare/aws/ecache/ElasticCacheSample.java
@@ -0,0 +1,14 @@
+package com.mlimavieira.mcare.aws.ecache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticCacheSample {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ElasticCacheSample.class, args);
+
+	}
+}
---
+++ aws-elastic-cache/src/main/java/com/mlimavieira/mcare/aws/ecache/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.mlimavieira.mcare.aws.ecache.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+}
