8a693005adf669116d7d2b34adf1afba9a99e02e
Marcio Vieira <mlimavieira@gmail.com>
2015-01-10 12:30:52 -0200

    S3 Metidos

--- aws-s3/src/main/java/com/mlimavieira/mcare/aws/s3/S3Sample.java
+++ aws-s3/src/main/java/com/mlimavieira/mcare/aws/s3/S3Sample.java
@@ -1,14 +1,39 @@
 package com.mlimavieira.mcare.aws.s3;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3Client;
 
 @SpringBootApplication
 public class S3Sample {
 
+	@Value("#{systemProperties['HANDSON_ACCESS_KEY']}")
+	private String ACCESS_KEY;
+	@Value("#{systemProperties['HANDSON_ACCESS_SECRET']}")
+	private String ACCESS_SECRET;
+
 	public static void main(String[] args) {
 
 		SpringApplication.run(S3Sample.class, args);
+
+	}
+
+	@Bean
+	public AmazonS3Client getAmazonS3Client() {
+		return new AmazonS3Client(new BasicAWSCredentials(ACCESS_KEY,
+				ACCESS_SECRET));
+	}
+
+	@PostConstruct
+	public void postConstruct() {
+		System.out.println(ACCESS_KEY);
+		System.out.println(ACCESS_SECRET);
 
 	}
 }
--- aws-s3/src/main/java/com/mlimavieira/mcare/aws/s3/core/BucketService.java
+++ aws-s3/src/main/java/com/mlimavieira/mcare/aws/s3/core/BucketService.java
@@ -1,26 +1,60 @@
 package com.mlimavieira.mcare.aws.s3.core;
 
+import java.io.ByteArrayInputStream;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CreateBucketRequest;
+import com.amazonaws.services.s3.model.DeleteBucketRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 
 @Component
 public class BucketService {
 
+	@Autowired
+	private AmazonS3Client clientS3;
 
+	public void createBucket(final String bucketName) {
 
-	// Access Key ID: AKIAI6NHXB445EET7PKA
-	// Secret Access Key: 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
+		if (StringUtils.isEmpty(bucketName)) {
+			throw new IllegalArgumentException("BucketName is required");
+		}
 
-
-	public void createBucket() {
-
+		final CreateBucketRequest bucketRequest = new CreateBucketRequest(bucketName);
+		clientS3.createBucket(bucketRequest);
 	}
 
-	public void deleteBucket(){
+	public void deleteBucket(final String bucketName) {
 
+		if (StringUtils.isEmpty(bucketName)) {
+			throw new IllegalArgumentException("BucketName is required");
+		}
+
+		final DeleteBucketRequest bucketRequest = new DeleteBucketRequest(bucketName);
+		clientS3.deleteBucket(bucketRequest);
 	}
 
-	public void putObject(){
+	public void putObject(final String bucketName, final String objectName, final byte[] bytes,
+			final String contentType) {
 
+		if (bytes.length == 0) {
+			throw new IllegalArgumentException("The parameter Bytes must be specified");
+		}
+
+		final ObjectMetadata metadata = new ObjectMetadata();
+		if (StringUtils.isEmpty(contentType)) {
+			metadata.setContentType(contentType);
+		}
+		metadata.setContentLength(bytes.length);
+
+		final PutObjectRequest request = new PutObjectRequest(bucketName, objectName,
+				new ByteArrayInputStream(bytes), metadata);
+
+		clientS3.putObject(request);
 	}
 
 	public void getObject() {
