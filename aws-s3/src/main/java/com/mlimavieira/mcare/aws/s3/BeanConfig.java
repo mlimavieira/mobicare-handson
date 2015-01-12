60dbf6b958a03e4890ddfa09cefc4bfd956c5306
Marcio Vieira <mlimavieira@gmail.com>
2015-01-12 17:25:07 -0200

    commit

--- aws-s3/src/main/java/com/mlimavieira/mcare/aws/s3/BeanConfig.java
+++ aws-s3/src/main/java/com/mlimavieira/mcare/aws/s3/BeanConfig.java
@@ -3,7 +3,6 @@ package com.mlimavieira.mcare.aws.s3;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.StringUtils;
 
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3Client;
@@ -19,12 +18,7 @@ public class BeanConfig {
 	@Bean
 	public AmazonS3Client getAmazonS3Client() {
 
-		if (StringUtils.isEmpty(ACCESS_KEY)) {
-			ACCESS_KEY = "AKIAI4LVAGHCUNKU3WOA";
-		}
-		if (StringUtils.isEmpty(ACCESS_SECRET)) {
-			ACCESS_SECRET = "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";
-		}
+
 		return new AmazonS3Client(new BasicAWSCredentials(ACCESS_KEY, ACCESS_SECRET));
 	}
 }
